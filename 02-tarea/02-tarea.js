8bc0e0378b65f8bfe4522393fdfabf8fe3489a76
Jose Melendez <jomelendezgm95@gmail.com>
2022-09-14 01:28:49 -0400

    Segundo ejercicio del curso de JavaScript

---
+++ 02-tarea/02-tarea.js
@@ -0,0 +1,23 @@
+//Lista con datos persoanles
+
+const lista_datos = {
+
+    Nombre: "Jose",
+    Edad: 26,
+    Desarrollador:true,
+    fecha_nacimiento:{
+        dia: 30,
+        mes: "septiembre",
+        anyo: 1995 
+    },
+    libro_favorito: {
+        
+        titulo: "El sutil arte de que (casi todo) te importe una mierda",
+        autor: "MARK MANSON",
+        fecha:2018,
+        link: "https://www.amazon.com/sutil-importe-mierda-HARPERCOLLINS-Spanish/dp/8491392289"
+    }
+
+}
+
+console.log(lista_datos)
