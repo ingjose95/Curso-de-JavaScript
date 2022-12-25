15a0acef10f1d4400efbdb6265713542367a6a5b
Jose Melendez <jomelendezgm95@gmail.com>
2022-12-25 14:11:30 -0400

    Decimo tercer ejercicio del curso de JavaScript

---
+++ 13-tarea/index.js
@@ -0,0 +1,9 @@
+// Utiliza los tres estilos de comillas de forma alterna (comillas simples, dobles, backticks)
+
+const nombre = "Fernando";
+
+const apellido = "Fernandez";
+
+const pais = "Japon";
+
+const estado = "Carabobo";
